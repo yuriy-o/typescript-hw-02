ff32721db9fded862f481ee29274b9bfb55116e4
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-09-08 12:43:45 +0300

    type unknown

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,20 @@
+// Однозначної відповіді нема, бо є декілька варіантів
+
+// 1) Зробити однаковими типи та забити
+
+// let some: unknown;
+// some = 'Text';
+// let str: unknown;
+// str = some;
+
+
+// 2) Додати перевірку
+
 let some:unknown;
 some = 'Text';
 let str: string;
-str = some;
+if (typeof some === 'string') {
+    str = some;
+}
 
 export {};
