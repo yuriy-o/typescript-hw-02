0e8d0b415ac08bfa5ee082820fa1830bdfe6804c
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-09-08 14:47:14 +0300

    two objects

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,20 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type PageDetails = {
+  createAt: Date;
+  updateAt: Date;
+};
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: PageDetails;
+};
+
+const page1: Page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -10,14 +23,14 @@ const page1 = {
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
-  }
-}
+  },
+};
 
-const page2 = {
+const page2: Page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
 
 export {};
