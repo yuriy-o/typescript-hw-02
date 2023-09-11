558966cc81863c7f386e003dc3e65e088ac44348
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-09-11 20:10:44 +0300

    інтерфейс, який описує пару ключ-значення

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,14 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
 export {};
+
+// Приклади використання
+const stringNumberPair: KeyValuePair<string, number> = { key: 'age', value: 25 };
+const stringBooleanPair: KeyValuePair<string, boolean> = { key: 'isActive', value: true };
+const numberArrayPair: KeyValuePair<number, string[]> = { key: 1, value: ['apple', 'banana'] };
