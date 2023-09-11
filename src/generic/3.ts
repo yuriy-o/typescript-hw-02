2ea5bacb4f56e152de2503de089a1a879858f138
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-09-11 14:45:53 +0300

    merge two objects

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,17 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+//? v1 →  Об'єднуємо objB в objA, але не повертаємо результат цієї операції. 
+//? Це може бути корисним, якщо ми хочемо змінити objA без створення нового об'єкта.
+function merge1<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-export {}
+//? v2 → Об'єднуємо objA і objB.
+//? Отримаємо об'єкт, що містить всі властивості як objA, так і objB.
+//? Це корисно, коли ми хочемо зберегти властивості обох об'єктів і ми впевнені, що вони не конфліктують.
+function merge2<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
+}
+
+export {};
